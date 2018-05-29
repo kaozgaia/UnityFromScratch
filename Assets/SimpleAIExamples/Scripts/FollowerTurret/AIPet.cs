a014c50651620557212115e81b01ccb988f627d0
Christian A. Fernández <kaozgaia@gmail.com>
2018-05-29 12:42:02 -0500

    Se agrega un ejemplo de un PetCube
    Se agrega un ejemplo de un PetCube

---
+++ Assets/SimpleAIExamples/Scripts/FollowerTurret/AIPet.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+namespace B2BGAI {
+    public class AIPet : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed = 2.0f;
+        [SerializeField]
+        private float accuracy = 5.0f;
+        [SerializeField]
+        private Transform objective;
+
+        // Update is called once per frame
+        void Update()
+        {
+            this.transform.LookAt(objective.position);
+            Vector3 direction = objective.position - this.transform.position;
+            if (direction.magnitude > accuracy)
+                this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
+
+
