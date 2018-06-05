2c65e0a0026b694923ef183ff754e2646afc4deb
Christian A. Fernández <kaozgaia@gmail.com>
2018-06-05 17:48:30 -0500

    Se agrega logic completa para simular un FOV con obstaculos

---
+++ Assets/AI_FOV/Scripts/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    [SerializeField]
+    private float moveSpeed = 5;
+
+    private Rigidbody _body;
+    private Camera _camera;
+    private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+        _body = GetComponent<Rigidbody>();
+        _camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _camera.transform.position.y));
+        transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        _body.MovePosition(_body.position + velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/AI_FOV/Scripts/FOV.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOV : MonoBehaviour {
+
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    [SerializeField]
+    private LayerMask targetMask;
+    [SerializeField]
+    private LayerMask obstacleMask;
+
+    private void Start()
+    {
+        StartCoroutine(FindTargetsWithDelay(0.02f));
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+	
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+
+        Collider[] targetsInViewRadious = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for(int i = 0; i< targetsInViewRadious.Length; i++)
+        {
+            Transform target = targetsInViewRadious[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if(Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float distToTarget = Vector3.Distance(transform.position, target.position);
+                if(!Physics.Raycast(transform.position, dirToTarget, distToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target); 
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleGlobal)
+    {
+        if (!angleGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+
+}
