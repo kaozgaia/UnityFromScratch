f260940a4d219b6f3acdba305e7905b560c5c493
Christian Fernandez <chris@hyperbeard.com>
2019-07-22 14:48:51 -0500

    added dictionary serializable to assembly def

---
+++ Core/SerializableDictionary/Example/Editor/UserSerializableDictionaryPropertyDrawers.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(StringStringDictionary))]
+[CustomPropertyDrawer(typeof(ObjectColorDictionary))]
+[CustomPropertyDrawer(typeof(StringGameobjectDictionary))]
+public class AnySerializableDictionaryPropertyDrawer : SerializableDictionaryPropertyDrawer {}
